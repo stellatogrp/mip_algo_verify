20c8902bda942c740312334512097c9897321c7d
Vinit Ranjan <vinitranjan821@gmail.com>
2024-11-10 22:09:07 -0500

    added DR to portfolio test with different K cone

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -8,8 +8,8 @@ np.set_printoptions(suppress=True)  # Suppress scientific notation
 
 
 def test_portfolio():
-    n = 3
-    d = 2
+    n = 10
+    d = 3
     gamma = 3
     lambd = 1e-4
 
@@ -17,6 +17,7 @@ def test_portfolio():
     F = np.random.normal(size=(n, d))
     Fmask = np.random.randint(0, high=2, size=(n, d))
     print(Fmask)
+    F = np.multiply(F, Fmask)
     Ddiag = np.random.uniform(high=np.sqrt(d), size=(n, ))
     D = np.diag(Ddiag)
 
@@ -53,17 +54,6 @@ def test_portfolio():
     q = np.zeros(n + d)
     q[:n] = -(mu + 2 * lambd * x_prev)
 
-    A = np.block([
-        [F.T, -np.eye(d)],
-        [np.ones((1, n)), np.zeros((1, d))],
-        [-np.eye(n), np.zeros((n, d))]
-    ])
-    b = np.hstack([np.zeros(d), 1, np.zeros(n)])
-    print(A)
-    print(b)
-
-    # assert A.shape == (n + d + 1, n + d)
-
     obj = .5 * cp.quad_form(z, P) + q.T @ z
     A1 = np.block([
         [F.T, -np.eye(d)]
@@ -91,21 +81,117 @@ def test_portfolio():
     z_triple_block = z.value[:n]
     print(z_triple_block)
 
+    A = np.block([
+        [F.T, -np.eye(d)],
+        [np.ones(n), np.zeros(d)],
+        [-np.eye(n), np.zeros((n, d))]
+    ])
+    b = np.hstack([np.zeros(d), 1, np.zeros(n)])
+    print(A)
+    print(b)
+
+    # assert A.shape == (n + d + 1, n + d)
+
     s = cp.Variable(n + d + 1)
     constraints = [
         A @ z + s == b,
-        s >= 0,
+        # s >= 0,
+        s[d+1:] >= 0,
         s[:d+1] == 0,
     ]
 
     prob = cp.Problem(cp.Minimize(obj), constraints)
-    prob.solve()
+    prob.solve(solver=cp.SCS)
     # print(z.value)
     # print(s.value)
 
     z_single_block = z.value[:n]
+    z_full = z.value
+    y_full = constraints[0].dual_value
     print(z_single_block)
+    print('full primal/dual vars:')
+    print(z_full)
+    print(y_full)
+    print('w:', b - A @ z_full)
+    print('dual feas:', P @ z_full + A.T @ y_full + q)
 
     assert np.linalg.norm(x_orig - x_reformed) <= 1e-7
     assert np.linalg.norm(x_reformed - z_triple_block) <= 1e-7
     assert np.linalg.norm(z_triple_block - z_single_block) <= 1e-7
+
+    # test with DR splitting
+    Am, An = A.shape
+    M = np.block([
+        [P, A.T],
+        [-A, np.zeros((Am, Am))]
+    ])
+    lhs = np.eye(Am + An) + M
+    c = np.hstack([q, b])
+    # sk = np.zeros(Am + An)
+    sk = np.hstack([z_full, y_full])
+
+    def proj(v):
+        l = Am + An - n
+        u = Am + An
+        v[l:u] = np.maximum(v[l:u], 0)
+        return v
+
+    print('---testing DR---')
+    K = 1000
+    print(c)
+    for _ in range(K):
+        # print('-')
+        # print(sk - c)
+        utilde = np.linalg.solve(lhs, sk - c)
+
+        # print(utilde)
+        # print(2*utilde - sk)
+        u = proj(2 * utilde - sk)
+        # print(u)
+        sk = sk + u - utilde
+    print('from DR:')
+    # print(sk)
+    z_DR = sk[:An]
+    y_DR = sk[An:]
+    print(z_DR)
+    print(y_DR)
+    print('w:', b - A @ z_DR)
+    print('dual feas:', P @ z_DR + A.T @ y_full + q)
+
+
+# def test_portfolio_nonnegDR():
+#     n = 3
+#     d = 2
+#     gamma = 3
+#     lambd = 1e-4
+
+#     np.random.seed(3)
+#     F = np.random.normal(size=(n, d))
+#     Fmask = np.random.randint(0, high=2, size=(n, d))
+#     print(Fmask)
+#     F = np.multiply(F, Fmask)
+#     Ddiag = np.random.uniform(high=np.sqrt(d), size=(n, ))
+#     D = np.diag(Ddiag)
+
+#     mu = np.random.normal(size=(n,))
+#     Sigma = F @ F.T + D
+#     x_prev = 1/n * np.ones(n)
+
+#     # reformed
+#     x = cp.Variable(n)
+#     y = cp.Variable(d)
+#     obj = cp.quad_form(x, gamma * D + lambd * np.eye(n)) + gamma * cp.sum_squares(y) - (mu + 2 * lambd * x_prev) @ x
+#     constraints = [cp.sum(x) == 1, x >= 0, y == F.T @ x]
+#     prob = cp.Problem(cp.Minimize(obj), constraints)
+#     prob.solve()
+#     x_reformed = x.value
+#     print(x_reformed)
+
+#     # block reformed
+#     z = cp.Variable(n + d)
+#     P = 2 * np.block([
+#         [gamma * D + lambd * np.eye(n), np.zeros((n, d))],
+#         [np.zeros((d, n)), gamma * np.eye(d)]
+#     ])
+#     q = np.zeros(n + d)
+#     q[:n] = -(mu + 2 * lambd * x_prev)
