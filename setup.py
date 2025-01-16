970729ca021866ac1524f55a3cadaa88432cdcfa
Vinit Ranjan <vinitranjan821@gmail.com>
2025-01-16 00:12:39 -0500

    removed setup.py in favor of pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-# TODO: add the license/version/etc like in pepit
-setup(
-    name="mipalgover",
-    version="0.0.1",
-    packages=find_packages()
-)
