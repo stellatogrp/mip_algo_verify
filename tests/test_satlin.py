19fbae72dc4193a8772fde62e08751c077975813
Vinit Ranjan <vinitranjan821@gmail.com>
2025-01-17 16:01:47 -0500

    added better satlin test for all three cases

--- tests/test_satlin.py
+++ tests/test_satlin.py
@@ -7,50 +7,55 @@ np.set_printoptions(precision=5)  # Print few decimal places
 np.set_printoptions(suppress=True)  # Suppress scientific notation
 
 
-def test_partial_nnqp():
-    np.random.seed(0)
+def test_satlin():
+    np.random.seed(8)
+
     n = 3
-    proj_ranges = (1, 3)
 
-    P = np.random.normal(size=(n, n))
-    P = P @ P.T
-    q = np.random.normal(size=(n,))
+    # P = np.random.normal(size=(n, n))
+    # P = P @ P.T
+    P = np.eye(n)
+    # q = np.random.normal(size=(n,))
+    q = np.zeros(n)
 
-    # testing cvxpy solution
+    # l = 0.5 * np.random.normal(size=(n,))
+    # u = l + 0.1
+    l = np.array([-1, 1, -0.1])
+    u = np.array([-0.9, 1.01, 0.1])
+
     x = cp.Variable(n)
     obj = .5 * cp.quad_form(x, P) + q.T @ x
-    constraints = [x[proj_ranges[0]: proj_ranges[1]] >= 0]
+    constraints = [x >= l, x <=u]
     prob = cp.Problem(cp.Minimize(obj), constraints)
+    res = prob.solve()
 
-    prob.solve()
-    print('test solution from cp:', x.value)
+    print('test solution from cvxpy:', x.value)
 
-    K = 1000
+    K = 100
     t = .01
-    zk = -np.ones(n)
+    z0_init = (l + u) / 2
+    zk = z0_init
     for _ in range(K):
-        # yk = (np.eye(n) - t * P) @ zk - t * q
         yk = zk - t * (P @ zk + q)
-        zk = partial_relu(yk, proj_ranges)
+        zk = proj(yk, l, u)
 
     print('solution from pgd:', zk)
-
     assert np.linalg.norm(x.value - zk) <= 1e-7
 
     VP = Verifier()
-
     q_offset = 0.1
     q_param = VP.add_param(n, lb=q-q_offset, ub=q+q_offset)
 
-    z0 = VP.add_initial_iterate(n, lb=-1, ub=-1)
-    K = 10
+    z0 = VP.add_initial_iterate(n, lb=z0_init, ub=z0_init)
+
+    K = 5
     z = [None for _ in range(K+1)]
     z[0] = z0
 
     all_res = []
     for k in range(1, K+1):
         print(k)
-        z[k] = VP.relu_step(z[k-1] - t * (P @ z[k-1] + q_param), proj_ranges=proj_ranges)
+        z[k] = VP.saturated_linear_step(z[k-1] - t * (P @ z[k-1] + q_param), l, u)
 
         VP.set_infinity_norm_objective(z[k] - z[k-1])
         res = VP.solve()
@@ -60,22 +65,18 @@ def test_partial_nnqp():
 
     print(f'VP resids: {all_res}')
 
-    zk = -np.ones(n)
+    zk = z0_init
     q = VP.extract_sol(q_param)
     PGD_resids = []
 
     for k in range(K):
-        znew = partial_relu(zk - t * (P @ zk + q), proj_ranges)
+        znew = proj(zk - t * (P @ zk + q), l, u)
         PGD_resids.append(np.max(np.abs(znew - zk)))
         zk = znew
 
     print(PGD_resids)
-
     assert np.all(np.array(PGD_resids) <= np.array(all_res) + 1e-7)
-    assert np.abs(PGD_resids[-1] - all_res[-1]) <= 1e-8
 
 
-def partial_relu(x, proj_ranges):
-    out = x.copy()
-    out[proj_ranges[0]: proj_ranges[1]] = np.maximum(x[proj_ranges[0]: proj_ranges[1]], 0)
-    return out
+def proj(v, l, u):
+    return np.minimum(np.maximum(v, l), u)
